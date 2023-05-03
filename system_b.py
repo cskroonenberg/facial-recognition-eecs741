0680ce63c7da5f49f7676f0360092474e1cf03bc
Caden Kroonenberg <cskroonenberg@gmail.com>
2023-05-03 15:25:52 -0500

    Add additional pre-processing and Euclidean distance to System B

--- system_b.py
+++ system_b.py
@@ -1,19 +1,31 @@
 import cv2
 import numpy as np
-from hamming import HammingDistance
+
+def EuclideanDistance(img_a, img_b):
+    # TODO: Calculate distance between images using some distance function
+    #COMPLETE(Michael): Calculate total Euclidean distance
+    distance = 0
+    for i in range(len(img_a)):
+        distance += np.sqrt(np.sum((img_a[i]-img_b[i])**2))
+    return distance
 
 def SystemBScoreMatrix(gallery_imgs, probe_imgs):
     # cv2.imshow('original gallery', gallery_imgs[1])
     # cv2.imshow('original probe', probe_imgs[1])
     # Crop mouth
+    filt_2d_kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
     for i, img in enumerate(gallery_imgs):
+        gallery_imgs[i] = cv2.filter2D(gallery_imgs[i].copy(), ddepth= -1, kernel= filt_2d_kernel)
         gallery_imgs[i] = gallery_imgs[i][0:33, :]
+        gallery_imgs[i] = cv2.GaussianBlur(gallery_imgs[i].copy(), (3,3), 0)
     for i, img in enumerate(probe_imgs):
+        probe_imgs[i] = cv2.filter2D(probe_imgs[i].copy(), ddepth= -1, kernel= filt_2d_kernel)
         probe_imgs[i] = probe_imgs[i][0:33, :]
+        probe_imgs[i] = cv2.GaussianBlur(probe_imgs[i].copy(), (3,3), 0)
 
 
-    # cv2.imshow('modified gallery', gallery_imgs[1])
-    # cv2.imshow('modified probe', probe_imgs[1])
+    #cv2.imwrite('modified_gallery.png', gallery_imgs[1])
+    #cv2.imwrite('modified_probe.png', probe_imgs[1])
 
     # Convert images to binary
     for i, img in enumerate(gallery_imgs):
@@ -21,8 +33,8 @@ def SystemBScoreMatrix(gallery_imgs, probe_imgs):
     for i, img in enumerate(probe_imgs):
         _, probe_imgs[i] = cv2.threshold(img,60,255,cv2.THRESH_BINARY)
 
-    # cv2.imshow('binarized gallery', gallery_imgs[1])
-    # cv2.imshow('binarized probe', probe_imgs[1])
+    #cv2.imwrite('binarized_gallery.png', gallery_imgs[1])
+    #cv2.imwrite('binarized_probe.png', probe_imgs[1])
 
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
@@ -30,6 +42,6 @@ def SystemBScoreMatrix(gallery_imgs, probe_imgs):
     A = np.empty((100,100))
     for i in range(100):
         for j in range(100):
-            A[i, j] = HammingDistance(probe_imgs[i], gallery_imgs[j])
+            A[i, j] = EuclideanDistance(probe_imgs[i], gallery_imgs[j])
     
     return A
