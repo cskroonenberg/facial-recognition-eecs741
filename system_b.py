2ffb901172266af3120b57c12ac18e92bc20bacf
Andrew Riachi <andrew.riachi@gmail.com>
2023-05-03 13:23:01 -0500

    set thresholding maxVal to 255

--- system_b.py
+++ system_b.py
@@ -12,17 +12,14 @@ def SystemBScoreMatrix(gallery_imgs, probe_imgs):
         probe_imgs[i] = probe_imgs[i][0:33, :]
 
 
-    cv2.imshow('modified gallery', gallery_imgs[1])
-    cv2.imshow('modified probe', probe_imgs[1])
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('modified gallery', gallery_imgs[1])
+    # cv2.imshow('modified probe', probe_imgs[1])
 
     # Convert images to binary
     for i, img in enumerate(gallery_imgs):
-        _, gallery_imgs[i] = cv2.threshold(img,60,1,cv2.THRESH_BINARY)
+        _, gallery_imgs[i] = cv2.threshold(img,60,255,cv2.THRESH_BINARY)
     for i, img in enumerate(probe_imgs):
-        _, probe_imgs[i] = cv2.threshold(img,60,1,cv2.THRESH_BINARY)
+        _, probe_imgs[i] = cv2.threshold(img,60,255,cv2.THRESH_BINARY)
 
     A = np.empty((100,100))
     for i in range(100):
