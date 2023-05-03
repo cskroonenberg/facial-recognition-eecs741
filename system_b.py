3caecc3a21c17d6f2c4bac2e091e4f351ecf38f2
Andrew Riachi <andrew.riachi@gmail.com>
2023-05-03 13:23:22 -0500

    comment out debugging views

--- system_b.py
+++ system_b.py
@@ -3,8 +3,8 @@ import numpy as np
 from hamming import HammingDistance
 
 def SystemBScoreMatrix(gallery_imgs, probe_imgs):
-    cv2.imshow('original gallery', gallery_imgs[1])
-    cv2.imshow('original probe', probe_imgs[1])
+    # cv2.imshow('original gallery', gallery_imgs[1])
+    # cv2.imshow('original probe', probe_imgs[1])
     # Crop mouth
     for i, img in enumerate(gallery_imgs):
         gallery_imgs[i] = gallery_imgs[i][0:33, :]
@@ -20,6 +20,12 @@ def SystemBScoreMatrix(gallery_imgs, probe_imgs):
         _, gallery_imgs[i] = cv2.threshold(img,60,255,cv2.THRESH_BINARY)
     for i, img in enumerate(probe_imgs):
         _, probe_imgs[i] = cv2.threshold(img,60,255,cv2.THRESH_BINARY)
+
+    # cv2.imshow('binarized gallery', gallery_imgs[1])
+    # cv2.imshow('binarized probe', probe_imgs[1])
+
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     A = np.empty((100,100))
     for i in range(100):
